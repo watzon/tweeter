a345b6dee3b7232b3076ac861ae0f27e20065fa1
Chris Watson <chris@watzon.me>
2018-03-01 04:24:31 -0700

    Hopefully last try

--- spec/tweeter/format_spec.cr
+++ spec/tweeter/format_spec.cr
@@ -2,8 +2,10 @@ require "../spec_helper"
 
 describe "Style" do
   it "code should be formatted correctly (crystal tool format)" do
-    dir = ENV["TRAVIS_BUILD_DIR"]? ? ENV["TRAVIS_BUILD_DIR"] + "/{src,spec}" : "./{src,spec}"
-    result = system("crystal tool format #{dir} --check")
-    expect(result).to be_true
+    root = ENV["TRAVIS_BUILD_DIR"]? ? ENV["TRAVIS_BUILD_DIR"] : "./"
+    src = system("crystal tool format #{root}/src --check")
+    spec = system("crystal tool format #{root}/spec --check")
+    expect(src).to be_true
+    expect(spec).to be_true
   end
 end
