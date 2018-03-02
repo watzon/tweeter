72d45e612d3d278d40ca27ef4c0535794c9dc3df
Chris Watson <chris@watzon.me>
2018-03-02 13:01:11 -0700

    Fixed arguments?

--- src/tweeter/arguments.cr
+++ src/tweeter/arguments.cr
@@ -6,9 +6,13 @@ module Tweeter
       args = args.to_a.flatten
       super(args.size)
 
-      @options = !args.empty? && (args.last.is_a?(::Hash) || args.last.is_a?(NamedTuple)) ?
-        args.pop.to_h.transform_keys(&.to_s).transform_values(&.to_s) :
-        {} of String => String
+      @options = if !args.empty? && args.last.is_a?(::Hash)
+                   args.pop.as(Hash(String, String))
+                 elsif !args.empty? && args.last.is_a?(::NamedTuple)
+                   args.pop.as(NamedTuple).to_h.transform_keys(&.to_s).transform_values(&.to_s)
+                 else
+                   {} of String => String
+                 end
 
       args.each { |a| a.is_a?(T) ? push(a) : nil }
     end
